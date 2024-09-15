c9a9b56bdb4756f422b3cc77113e5b02eb9a51e4
Typicalsleepingboy.exe <99948517+Typicalsleepingboy@users.noreply.github.com>
2024-09-15 19:00:28 +0700

    Update liveController.js

--- controllers/liveController.js
+++ controllers/liveController.js
@@ -5,8 +5,12 @@ const getJKT48Lives = async (req, res) => {
     try {
         const data = await fetchData('https://www.showroom-live.com/api/live/onlives');
         
+        // Filter for rooms starting with either 'JKT48' or 'officialJKT48'
         const jkt48Lives = uniqueBy(data.onlives.flatMap((genre) =>
-            genre.lives.filter((live) => live.room_url_key && live.room_url_key.startsWith('JKT48'))
+            genre.lives.filter((live) => 
+                live.room_url_key && 
+                (live.room_url_key.startsWith('JKT48') || live.room_url_key.startsWith('officialJKT48'))
+            )
         ), 'live_id');
         
         // If there are no JKT48 live streams, return an empty array
